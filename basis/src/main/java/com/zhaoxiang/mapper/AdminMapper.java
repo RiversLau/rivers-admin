0616c61f312ffbc2bff3d7ffc961189009fdb881
LiuRivers <zhaoxiang0805@hotmail.com>
2017-08-27 18:33:14 +0800

    项目初始化

--- basis/src/main/java/com/zhaoxiang/mapper/AdminMapper.java
+++ basis/src/main/java/com/zhaoxiang/mapper/AdminMapper.java
@@ -1,10 +1,14 @@
 package com.zhaoxiang.mapper;
 
+import com.zhaoxiang.vo.AdminVO;
+
+import java.util.Map;
+
 /**
  * Author: Rivers
  * Date: 2017/8/27 15:57
  */
 public interface AdminMapper {
 
-
+    AdminVO getStatusEqualUserByUsername(Map<String, Object> params);
 }
--- basis/src/main/java/com/zhaoxiang/service/impl/AdminServiceImpl.java
+++ basis/src/main/java/com/zhaoxiang/service/impl/AdminServiceImpl.java
@@ -1,7 +1,9 @@
 package com.zhaoxiang.service.impl;
 
+import com.zhaoxiang.mapper.AdminMapper;
 import com.zhaoxiang.service.AdminService;
 import com.zhaoxiang.vo.AdminVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,9 +13,13 @@ import org.springframework.stereotype.Service;
 @Service("/adminService")
 public class AdminServiceImpl implements AdminService {
 
+    @Autowired
+    private AdminMapper adminMapper;
+
     @Override
     public AdminVO getEnabledUserByUsername(String username) {
 
-        return null;
+        AdminVO admin = adminMapper.getStatusEqualUserByUsername(null);
+        return admin;
     }
 }
