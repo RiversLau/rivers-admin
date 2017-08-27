d01c20d491eea319465541d58b94af9c04cfcb63
LiuRivers <zhaoxiang0805@hotmail.com>
2017-08-27 16:45:57 +0800

    项目初始化

--- admin/src/main/java/com/zhaoxiang/auth/FilterDefinitionMap.java
+++ admin/src/main/java/com/zhaoxiang/auth/FilterDefinitionMap.java
@@ -1,38 +1,1 @@
-package com.zhaoxiang.auth;
-
-import org.springframework.beans.factory.FactoryBean;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-/**
- * Author: RiversLau
- * Date: 2017/8/17 15:15
- */
-public class FilterDefinitionMap implements FactoryBean<Map<String, String>> {
-
-    @Override
-    public Map<String, String> getObject() throws Exception {
-
-        return getAllAuthorityRouteMap();
-    }
-
-    @Override
-    public Class<?> getObjectType() {
-
-        return Map.class;
-    }
-
-    @Override
-    public boolean isSingleton() {
-
-        return true;
-    }
-
-    private Map<String, String> getAllAuthorityRouteMap() {
-
-        Map<String, String> list = new LinkedHashMap<String, String>();
-
-        return list;
-    }
-}
+package com.zhaoxiang.auth;import org.springframework.beans.factory.FactoryBean;import java.util.LinkedHashMap;import java.util.Map;/** * Author: RiversLau * Date: 2017/8/17 15:15 */public class FilterDefinitionMap implements FactoryBean<Map<String, String>> {    @Override    public Map<String, String> getObject() throws Exception {        return getAllAuthorityRouteMap();    }    @Override    public Class<?> getObjectType() {        return Map.class;    }    @Override    public boolean isSingleton() {        return true;    }    private Map<String, String> getAllAuthorityRouteMap() {        Map<String, String> list = new LinkedHashMap<String, String>();        return list;    }}
