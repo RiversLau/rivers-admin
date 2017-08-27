6ddb0696f8d4845966fdff5b6630949e7d123edf
LiuRivers <zhaoxiang0805@hotmail.com>
2017-08-27 18:33:37 +0800

    项目初始化

--- admin/src/main/java/com/zhaoxiang/controller/AdminCtrl.java
+++ admin/src/main/java/com/zhaoxiang/controller/AdminCtrl.java
@@ -1,10 +1,52 @@
 package com.zhaoxiang.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
 /**
  * Author: Rivers
  * Date: 2017/8/27 16:01
  */
-public class AdminCtrl extends BaseCtrl {
+@Controller
+@RequestMapping("/user")
+public class AdminCtrl {
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String userLogin(@RequestParam Map<String, String> params) {
 
+        String username = params.get("username");
+        String password = params.get("password");
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+        Subject subject = SecurityUtils.getSubject();
+        if (!subject.isAuthenticated()) {
+            try {
+                subject.login(token);
+                Session session = subject.getSession();
+                session.setAttribute("user", username);
+            } catch (AuthenticationException e) {
+                throw new UnknownAccountException("用户名或密码错误");
+            }
+        }
+
+        return "home";
+    }
+
+    @RequestMapping("/logout")
+    public String userLogout() {
+
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        return "redirect:/";
+    }
 }
--- admin/src/main/java/com/zhaoxiang/controller/BaseCtrl.java
+++ admin/src/main/java/com/zhaoxiang/controller/BaseCtrl.java
@@ -1,10 +1,18 @@
 package com.zhaoxiang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * Author: Rivers
- * Date: 2017/8/27 16:01
+ * Author: RiversLau
+ * Date: 2017/8/17 15:08
  */
+@Controller
 public class BaseCtrl {
 
+    @RequestMapping("/")
+    public String redirectLogin() {
 
+        return "login";
+    }
 }
